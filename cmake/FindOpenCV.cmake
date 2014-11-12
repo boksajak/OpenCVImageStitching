c9c921aa86e701f5da42966f87708560686e66d4
boksajak <jakub.boksansky@gmail.com>
2014-11-12 23:10:09 +0100

    opencv linking

--- cmake/FindOpenCV.cmake
+++
@@ -1,5 +0,0 @@
-
-#find_path(OpenCV_INCLUDE_DIR opencv.hpp)
-
-find_library(OpenCV_LIBRARIES
-  PATH_SUFFIXES opencv)
--- source/ImageStitching.cpp
+++ source/ImageStitching.cpp
@@ -1,11 +1,6 @@
 #include "common.h"
 #include "ImageStitching.h"
 
-// DevIL Library Includes
-//#include "il.h"
-//#include "ilu.h"
-//#include "ilut.h"
-
 // OpenCV
 #include <opencv2/opencv.hpp>
 
@@ -15,16 +10,13 @@ namespace ImageStitching {
 	Image StitchImages(std::vector<std::string> &inputImages) {
 		Image result;
 
+		std::vector<cv::Mat> loadedImages;
+
 		for (size_t i = 0; i < inputImages.size(); i++) {
-			cv::Mat image;
-			image = cv::imread( inputImages[i], 1 );
-
-
-			//ILuint ImageName;
-			//ilGenImages(1, &ImageName);
-			//ilBindImage(ImageName);
-			//ilLoadImage(inputImages[i].c_str()); 
+			loadedImages.push_back(cv::imread(inputImages[i], 1));
 		}
+
+		cv::imshow("first image",loadedImages[0]);
 
 		return result;
 	}
