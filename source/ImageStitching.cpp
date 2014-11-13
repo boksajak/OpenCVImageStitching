783248ddaf636b4fe8cb6caeb10ad31d26388f8d
jakub.boksansky@gmail.com <jakub.boksansky@gmail.com>
2014-11-13 11:57:45 +0100

    Stitching

--- source/ImageStitching.cpp
+++ source/ImageStitching.cpp
@@ -1,8 +1,9 @@
 #include "common.h"
 #include "ImageStitching.h"
 
-// OpenCV
+// OpenCV includes
 #include <opencv2/opencv.hpp>
+#include "opencv2/stitching/stitcher.hpp"
 
 namespace ImageStitching {
 
@@ -13,11 +14,33 @@ namespace ImageStitching {
 		std::vector<cv::Mat> loadedImages;
 
 		for (size_t i = 0; i < inputImages.size(); i++) {
-			loadedImages.push_back(cv::imread(inputImages[i], 1));
+			cv::Mat img = cv::imread(inputImages[i], 1);
+
+			// downsample to 1/4
+			//resize(img,img,cv::Size(img.size().width * 0.25f, img.size().height * 0.25f));//resize image
+
+			loadedImages.push_back(img);
 		}
 
-		cv::imshow("first image",loadedImages[0]);
+		cv::Mat resultImg;
+		cv::Stitcher stitcher = cv::Stitcher::createDefault();
+		cv::Stitcher::Status status = stitcher.stitch(loadedImages, resultImg);
+
+		if (status != cv::Stitcher::OK)
+		{
+			TRACE_ERROR("Can't stitch images, error code = " << status << std::endl);
+		}
+
+		std::vector<int> compression_params;
+		compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+		compression_params.push_back(9);
+
+		imwrite("result.png", resultImg, compression_params);
+
+		resize(resultImg,resultImg,cv::Size(resultImg.size().width * 0.1f, resultImg.size().height * 0.1f));//resize image
+		cv::imshow( "Result", resultImg );
 
 		return result;
+
 	}
 }
